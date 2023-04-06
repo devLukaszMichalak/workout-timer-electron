fca4667d83f7d98047c84b817f657ddf0cb70b06
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2023-04-06 21:21:34 +0200

    better menu

---
+++ electron/menuTemplate.js
@@ -0,0 +1,54 @@
+const {app, BrowserWindow, shell, dialog} = require('electron');
+
+const isMac = process.platform === 'darwin'
+
+const aboutMenuItem = {
+  label: 'About Workout Timer', click: () => {
+    const message = `Workout Timer v0\n\nCreated by Łukasz Michalak`;
+    dialog.showMessageBox({title: 'About', message: message, buttons: ['OK']});
+  }
+}
+
+const resetTimerMenuItem = {
+  label: 'Reset timer', click: () => {
+    const win = BrowserWindow.getFocusedWindow();
+    win.webContents.send('reset-timer-item-clicked');
+  }
+}
+
+const sourceCodeMenuItem = {
+  label: 'Source code',
+  click: async () => {
+    await shell.openExternal('https://github.com/devLukaszMichalak/workout-timer-electron')
+  }
+}
+
+const
+  menuTemplate = [
+    {
+      label: 'File',
+      submenu: [
+        isMac ? {role: 'close'} : {role: 'quit'},
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [resetTimerMenuItem]
+    },
+    {
+      role: 'help',
+      submenu: [
+        sourceCodeMenuItem,
+        ...(!isMac ? [aboutMenuItem] : [])
+      ]
+    }
+  ]
+
+if (isMac) {
+  menuTemplate.unshift({
+    label: app.name,
+    submenu: [aboutMenuItem]
+  })
+}
+
+module.exports.menuTemplate = menuTemplate;
--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const {app, BrowserWindow, Menu, dialog} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const url = require("url");
 const path = require("path");
+
+const menuTemplate = require('./electron/menuTemplate').menuTemplate;
+
+// Create the menu from the menuTemplate and set the menu as the application menu
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
 
 let mainWindow
 
@@ -24,12 +30,8 @@ function createWindow() {
     })
   );
 
-  // Create the menu from the menuTemplate and set the menu as the application menu
-  const menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
-
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null
@@ -45,40 +47,3 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   if (mainWindow === null) createWindow()
 })
-
-const menuTemplate = [
-  {
-    label: 'File',
-    submenu: [
-      {role: 'quit'}
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Reset timer', click: () => {
-          const win = BrowserWindow.getFocusedWindow();
-          win.webContents.send('reset-timer-item-clicked');
-        }
-      }
-    ]
-  },
-  {
-    label: 'Help',
-    submenu: [
-      {
-        label: 'About', click: () => {
-          const {version, author} = require('./package.json');
-          const message = `Workout Timer v${version}\n\nCreated by ${author.name}`;
-          dialog.showMessageBox({title: 'About', message: message, buttons: ['OK']});
-        }
-      }
-    ]
-  }
-];
-
-//if mac add empty object to menu
-if (process.platform === 'darwin') {
-  menuTemplate.unshift({})
-}
